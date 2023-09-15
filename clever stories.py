e42774fd9a769fbf0bb76d2ad4c902d5176bebae
jaredmartinezdiaz <jhmd_521@icloud.com>
2023-09-15 13:58:05 -0600

    clever stories

---
+++ clever stories.py
@@ -0,0 +1,70 @@
+# I had a lot of fun doing this project, although it represents a challenge for me because everything 
+# I am learning is new, and it is my first time programming but I am happy to be able to achieve it. 
+# Below I will give you the words that you use to be able to run my program
+# Adjective: Angry
+# Animal: Lion
+# Verb: Eat
+# Exclamation: Ow, Ouch
+# Verb1: Run
+# Verb2: Sleep
+# Below I will post what I added to the story:
+
+print("Plese enter the following:")
+print()
+adjective = input("adjective: ")
+animal = input("animal: ")
+verb = input("verb: ")
+exclamation = input("exclamation: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+print()
+print("Your story is:")
+print()
+print("The otherday, I was really in trouble. It all started when I saw a very")
+print(f'{adjective.capitalize()}, {animal.capitalize()}, {verb}, down the hallway. "{exclamation.capitalize()}!", I yelled. But all')
+print(f"I could think to do was to {verb1}, over and over. Miraculously, ")
+print(f"that caused it to stop, but no before it tried to {verb2}")
+print("right in front of my family.")
+# For this second part add a little to the story and some more verbs I give you the words below 
+# Adjective: Angry
+# Animal: Lion
+# Verb: Eat
+# Exclamation: Ow, Ouch
+# Verb1: Run
+# Verb2: Sleep
+# Verb3: Left
+# Verb4: looking
+# Verb5: Thinking
+# Exclamation: Food
+# Verb6: See
+# Noun: Fear
+# Adjective: Happy
+# Below I will post what I added to the story:
+
+print("Plese enter the following:")
+print()
+adjective = input("adjective: ")
+animal = input("animal: ")
+verb = input("verb: ")
+exclamation = input("exclamation: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+verb3 = input("verb: ")
+verb4 = input("verb: ")
+verb5 = input("verb: ")
+exclamation2 = input("exclamation: ")
+verb6 = input("verb: ")
+noun = input("noun: ")
+adjective = input("adjective: ")
+print()
+print("Your story is:")
+print()
+print("The otherday, I was really in trouble. It all started when I saw a very")
+print(f'{adjective.capitalize()}, {animal.capitalize()}, {verb}, down the hallway. "{exclamation.capitalize()}!", I yelled. But all')
+print(f"I could think to do was to {verb1}, over and over. Miraculously, ")
+print(f"that caused it to stop, but no before it tried to {verb2}")
+print(f"right in front of my family. So we {verb3}, in silence {verb4} at her majestic mane and {verb5}")
+print(f'That we could have been her "{exclamation2.capitalize()}"!')
+print(f"From now on we will only {verb6} him behind a cage, whitout {noun} and {adjective}")
+
+# Thank you 
