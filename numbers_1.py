c39b25e48d5b14393ba4916dcb71eeaeec53cdf8
Jaredh24m <mtzjh24@icloud.com>
2023-09-18 11:52:42 -0600

    numbers

---
+++ numbers_1.py
@@ -0,0 +1,61 @@
+# pi = 3.14159
+# print (pi)
+
+# first_num = input("please enter a number ")
+# second_num = input("please enter another number ")
+# print(float(first_num) + float(second_num))
+
+# days_in_february = 28
+# print(str(days_in_february)  +  " total days in february")
+
+# color = "blue"
+# animal = "horse"
+# print(color + animal)
+# combine_words = color + " " + animal + "!"
+# print(combine_words)
+
+# boys_count = 10
+# girls_count = 12
+# print(boys_count + girls_count)
+# total_count = boys_count + girls_count
+# print(total_count)
+
+# length_num = 50
+# width_num = 10
+# print(length_num + width_num)
+# length_num = str(length_num)
+# width_num = str(width_num)
+# print(length_num + width_num)
+
+# quantity_from_user = input("How many items do you have? ")
+# quantity_number = int(quantity_from_user)
+# double_number = quantity_number * 2
+# print("Twice as many is " + str(double_number))
+
+# people_string = input("How many people are in the room? ")
+# people_number = int(people_string)
+# people_number = int(input("How many people are in the room? "))
+# print("In the room are " + str(int(input("How many people are in the room? "))) + " people")
+
+# user_age = int(input("How old are you? "))
+# print("On the next birhtday, you will be " + str(int(input("How old are you? ")) + 1) )
+
+# eggs = int(input("How many egg cartons do you have? "))
+# print ("You have " + str(int(input("How many egg cartons do you have? ")) * 12) + " egss")
+
+# cookies = float(input("How many cookies do you have? "))
+# people = float(input("How many people aret there? "))
+# print("Each person my have " + str(float(input("How many cookies do you have? ")) / float(input("How many people aret there? "))) + " cookies")
+
+# age = int(input("How old are you? "))
+# next_year_age = age + 1
+# print(f"On your next birthday, you will be {age + 1}. ")
+
+# cartons = int(input("How many egg carton do you have? "))
+# eggs = cartons * 12
+# print (f"You have {eggs} eggs")
+
+cookies = int(input("\nHow many cookies do you have? "))
+people = int(input("How many people are there? "))
+cookies_per_person = cookies / people
+print(f"Each person my have {cookies / people} cookies")
