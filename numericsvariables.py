fc5f70022ca6ba146c95db82183c26a3fe27452b
Jaredh24m <mtzjh24@icloud.com>
2023-09-18 11:53:07 -0600

    numericvariables

---
+++ numericsvariables.py
@@ -0,0 +1,12 @@
+age = int(input("How old are you? "))
+next_year_age = age + 1
+print(f"On your next birthday, you will be {age + 1}. ")
+print()
+cartons = int(input("How many egg carton do you have? "))
+eggs = cartons * 12
+print (f"You have {eggs} eggs")
+
+cookies = int(input("\nHow many cookies do you have? "))
+people = int(input("How many people are there? "))
+cookies_per_person = cookies / people
+print(f"Each person my have {cookies / people} cookies")
